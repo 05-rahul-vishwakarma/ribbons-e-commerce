c412719e07e191c30a072976ae509465f69b6800
930-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-11-22 01:13:37 +0530

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import Carousel from '@/app/commonComponents/home/Carousel'
+import Footer from '../commonComponents/Footers/Footer';
 export default function Home() {
    return (
       <section>
          <Carousel /> 
+        <Footer />
       </section>
    );
 }
